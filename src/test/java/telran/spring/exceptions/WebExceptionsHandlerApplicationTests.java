c6d67b660d83b7b989f179ea57b41410667b0391
Yuragranovsky <yuragranovsky@gmail.com>
2023-09-14 13:59:47 +0300

    WEB Global Exceptions Handler

---
+++ src/test/java/telran/spring/exceptions/WebExceptionsHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.spring.exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes= {GlobalExceptionsHandler.class})
+class WebExceptionsHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
